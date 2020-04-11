7067698f5274b2ecff1c786001cb7ea5f5134dae
Clay <wcl58981298@gmail.com>
2020-04-11 18:08:38 +0800

    add dev server, build dev dll

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,77 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        oneOf: [
-          {
-            loader: 'ts-loader',
-          },
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
+module.exports = (env) => {
+  const config = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', 'jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          oneOf: [
+            {
+              loader: 'ts-loader',
+            },
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      /**
+       * Q：Cannot find module './dll/vendor-manifest.json'
+       * A：npm run build:dll 需提前构建 dll
+       */
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: require('./dll/vendor-manifest.json'),
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new webpack.DllReferencePlugin({
-      context: __dirname,
-      manifest: require('./dist/vendor-manifest.json')
-    })
-  ],
+  };
+  if (env && env.NODE_ENV === 'development') {
+    config.plugins.push(
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: require('./dll/devserver-manifest.json'),
+      }),
+      new HtmlWebpackTagsPlugin({
+        append: false,
+        scripts: [
+          'dll/vendor.dll.js',
+          'dll/devserver.dll.js',
+        ],
+      })
+    );
+  } else {
+    config.plugins.push(
+      new HtmlWebpackTagsPlugin({
+        append: false,
+        scripts: [
+          './vendor.dll.js',
+        ],
+      }),
+      new CopyPlugin([
+        'dll/vendor.dll.js',
+      ]),
+    );
+  }
+  
+  return config;
 };
--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -4,16 +4,26 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'production',
   entry: {
+    devserver: [
+      'webpack-dev-server/client/index.js',
+      'webpack-dev-server/client/socket.js',
+      'webpack-dev-server/client/overlay.js',
+      'webpack-dev-server/client/utils/log.js',
+      'webpack-dev-server/client/utils/sendMessage.js',
+      'webpack-dev-server/client/utils/reloadApp.js',
+      'webpack-dev-server/client/utils/createSocketUrl.js',
+      'strip-ansi/index.js',
+    ],
     vendor: ['react','react-dom']
   },
   output: {
-    path: path.join(__dirname, './dist'),
+    path: path.join(__dirname, './dll/'),
     filename: '[name].dll.js',
     library: '[name]'
   },
   plugins: [
     new webpack.DllPlugin({
-      path: path.join(__dirname, 'dist/[name]-manifest.json'),
+      path: path.join(__dirname, '/dll/[name]-manifest.json'),
       name: '[name]'
     })
   ]
